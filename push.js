4e86caf9dea0c5d1a78dfa84eb5ec5b2dc4c97b8
John <hchoi375@gatech.edu>
2022-10-22 17:15:50 -0400

    ss

--- push.js
+++ push.js
@@ -3,8 +3,10 @@ var push = require('web-push')
 let vapidKeys = {
     publicKey: 'BLcRzm0Z2FDmsCUX2H_rCo2B2bT9urwLaTcIRFimO3JvuKSn7GEOq-M6IUHv9H7CD-ErH6HH3y89Un0KV-CzV_w',
     privateKey: 'yrYizttKpc5-cBnjdlsod8fZjBDSiy-FK_L3xqx-_64'
-  }
+}
 
-push.setVapidDetails('mailto:test#code.co.uk', vapidKeys.publicKey, vapidkeys.privateKey)
+push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidkeys.privateKey)
+
+let sub={};
 
 push.sendNotification(sub, 'testmessage')
