1391762d145af20ad965a8d2062101c1781151ba
John <hchoi375@gatech.edu>
2022-10-22 16:34:10 -0400

    Initial

---
+++ push.js
@@ -0,0 +1,6 @@
+var push = require('web-push')
+
+let vapidKeys = {
+    publicKey: 'BLcRzm0Z2FDmsCUX2H_rCo2B2bT9urwLaTcIRFimO3JvuKSn7GEOq-M6IUHv9H7CD-ErH6HH3y89Un0KV-CzV_w',
+    privateKey: 'yrYizttKpc5-cBnjdlsod8fZjBDSiy-FK_L3xqx-_64'
+  }
