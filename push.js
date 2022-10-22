c871d6b6b19a0ae38912032edf066221aba2e24e
John <hchoi375@gatech.edu>
2022-10-22 18:02:59 -0400

    Test

--- push.js
+++ push.js
@@ -3,16 +3,15 @@ var push = require('web-push')
 let vapidKeys = {
     publicKey: 'BLcRzm0Z2FDmsCUX2H_rCo2B2bT9urwLaTcIRFimO3JvuKSn7GEOq-M6IUHv9H7CD-ErH6HH3y89Un0KV-CzV_w',
     privateKey: 'yrYizttKpc5-cBnjdlsod8fZjBDSiy-FK_L3xqx-_64'
-}
+};
 
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub={
-    endpoint:"https://fcm.googleapis.com/fcm/send/cb4sMTBonFU:APA91bFGhIHo_cCQY2yHzt8GxqDWY785TXQewkp3kFhCCXKA_0oNqpzQ7TaJp7Nwy66LsWSRvQj4ISLurBzGrwg2vpw1sMhjsCl9R4p4SwGrIiw_Gj4bS8DoXNEqii16N4O7kEPVM-eL",
-    expirationTime:null,
-    keys:{
-        p256dh:"BE3QyXP3UvFHEYDdabdbqmdCito8C2yf0NmauS-XG8qORgInrpN1_rJ6dh1xrfNSEt8HRmpea2ssthaePhbwgN8",
-        auth:"iZ3SJ0i0cpFuQ-c_PsWnDw"
+let sub={endpoint:"https://fcm.googleapis.com/fcm/send/cxI1_hiKMJw:APA91bF8tM7AgW70TL19yD4DCCN8i4mw8Fg18PK8jdz4KBKAoAeLykYAwZ6HQLyzdV4OOz46ELom8d77S-mxlc9FNPuzTh1wA1ldUkoaHVv9z7PylTqfAfKs8xUxjL1RTNLX4YtDTrIj", 
+    expirationTimem: null, 
+    keys :{
+        p256dh:"BOieGhRzqWOhCYJeWoPJBFsxdDG1Z0rUAY2lFvN3rO4lw-P7qEe9m2LghCQKeDwCA1JZs4rVUHPp4GvkeToCrFA",
+        auth:"PNUDapHhohMUJqaXpCu32Q"
     }
 };
 
--- sw.js
+++ sw.js
@@ -1,22 +1,20 @@
 self.addEventListener('push', function(event) {
-    const analyticsPromise = pushReceivedTracking();
-    const pushInfoPromise = fetch('/api/get-more-data')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        const title = 'John says...';
-        const message = 'Hi';
-
-        return self.registration.showNotification(title, {
-        body: message
-        });
-    });
-
-    const promiseChain = Promise.all([
-    analyticsPromise,
-    pushInfoPromise
-    ]);
-
-    event.waitUntil(promiseChain);
+    var options = {
+        body: 'This notification was generated from a push!',
+        vibrate: [100, 50, 100],
+        data: {
+            dataOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {
+                action: 'explore',
+                title: 'explore this new world'
+            },
+            {
+                action: 'close', title: 'Close'
+            }
+        ]
+    };
+    event.waitUntil(self.ServiceWorkerRegistration.showNotification('Hello world!', otopins));
 });
