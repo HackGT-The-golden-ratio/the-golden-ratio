c6342f3267b94568742e0508effecf0a01253722
sbae89 <43897776+harrysedev@users.noreply.github.com>
2022-10-23 03:59:01 -0400

    sound, d3 init

---
+++ predict.js
@@ -0,0 +1,156 @@
+
+    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
+
+    // the link to your model provided by Teachable Machine export panel
+    const URL = "https://teachablemachine.withgoogle.com/models/jPZxS8HQl/";
+    let model, webcam, ctx, labelContainer, maxPredictions;
+
+    var audio = new Audio('glass.wav');
+
+    addEventListener('load', async () => {
+      let sw = await navigator.serviceWorker.register('./sw.js')
+      console.log(sw)
+    })
+
+    var date1 = new Date();
+
+    async function init() {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // Note: the pose library adds a tmPose object to your window (window.tmPose)
+        model = await tmPose.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const width = 486;
+        const height = 380;
+
+        // const size = 200;
+        const flip = true; // whether to flip the webcam
+        webcam = new tmPose.Webcam(width, height, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        date1 = new Date()
+        window.requestAnimationFrame(loop);
+
+        // append/get elements to the DOM
+        const canvas = document.getElementById("canvas");
+        canvas.width = width; canvas.height = height;
+        ctx = canvas.getContext("2d");
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
+    }
+
+    async function loop(timestamp) {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+    var number = 0;
+    
+
+    async function predict() {
+      // Prediction #1: run input through posenet
+      // estimatePose can take in an image, video or canvas html element
+      const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+      // Prediction 2: run input through teachable machine classification model
+      const prediction = await model.predict(posenetOutput);
+  
+      for (let i = 0; i < maxPredictions; i++) {
+          const classPrediction =
+              prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+          labelContainer.childNodes[i].innerHTML = classPrediction;
+      }
+      
+      var max_num = Math.max(prediction[0].probability.toFixed(2), prediction[1].probability.toFixed(2), prediction[2].probability.toFixed(2));
+      for (let i = 0; i < maxPredictions; i++) {
+          if (prediction[i].probability.toFixed(2) == max_num){
+              var cur_class = i
+          }
+      }
+      // finally draw the poses
+      drawPose(pose);
+      var date2 = new Date();
+      time_diff = diff_minutes(date1, date2);
+      if (cur_class == 0){
+          date1 = new Date();
+      }
+
+      if (time_diff > 5){
+          alertUser();
+      }
+      number++;
+      if (number % 15 == 0) {
+        drawGraph(prediction);
+      }
+  }
+
+    function drawGraph(prediction) {
+      data = 100 * prediction[0].probability;
+      var color = "#C44A53";
+
+      if (data < 30) {
+        color = "#C44A53";
+      } else if (data < 60) {
+        color = "#F1B16E";
+      } else if (data < 85) {
+        color = "#7DBFA7";
+      } else {
+        color = "#4B86B8";
+      }
+
+      var obj = {
+        value: 50,
+        time: new Date(),
+        color: color,
+        type: "rect"
+      };
+
+      // send the datum to the chart
+      chart.datum(obj);
+    }
+
+    function drawPose(pose) {
+        if (webcam.canvas) {
+            ctx.drawImage(webcam.canvas, 0, 0);
+            // draw the keypoints and skeleton
+            if (pose) {
+                const minPartConfidence = 0.5;
+                tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+                tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+            }
+        }
+    }
+
+    async function subscribe() {
+      let sw = await navigator.serviceWorker.ready;
+      let push = await sw.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: 
+              'BLcRzm0Z2FDmsCUX2H_rCo2B2bT9urwLaTcIRFimO3JvuKSn7GEOq-M6IUHv9H7CD-ErH6HH3y89Un0KV-CzV_w'
+      })
+      console.log(JSON.stringify(push))
+  }
+
+  function waitforme(milisec) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve('') }, milisec);
+    })
+  }
+
+  function alertUser() {
+      audio.play();
+      alert('Please seat upright!');
+      date1 = new Date();
+  }
+
+  function diff_minutes(dt2, dt1) {
+      var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+      return Math.abs(Math.round(diff));
+  }
