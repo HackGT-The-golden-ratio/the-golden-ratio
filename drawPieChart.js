bb1a7abbc5a01a194ad915010ce8a5b6b10a1d23
junggeehoon <junggeehoon@gmail.com>
2022-10-23 07:29:23 -0400

    chart added

--- drawPieChart.js
+++ drawPieChart.js
@@ -7,11 +7,11 @@ var population = [
 var chart = PieChart(population, {
   name: d => d.name,
   value: d => d.value,
-  width: 200,
-  height: 200,
+  width: 350,
+  height: 350,
   colors: ["#4BD0D4", "#4CD446", "#D42D40"]
 })
 
-d3.selectAll(".stack-chart").select(function() {
+d3.selectAll(".stack-down").select(function() {
   return this.appendChild(chart);
 });
