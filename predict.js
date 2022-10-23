95872db3c80204db7d9e2ae4c2942884b08f1c4c
John <hchoi375@gatech.edu>
2022-10-23 06:44:02 -0400

    sdf

--- predict.js
+++ predict.js
@@ -1,6 +1,5 @@
 
     // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/pose
-    import { pushNot } from './push.js';
     // the link to your model provided by Teachable Machine export panel
     const URL = "https://teachablemachine.withgoogle.com/models/jPZxS8HQl/";
     let model, webcam, ctx, labelContainer, maxPredictions;
@@ -14,7 +13,7 @@
 
     var date1 = new Date();
 
-    export async function init() {
+    async function init(){
         const modelURL = URL + "model.json";
         const metadataURL = URL + "metadata.json";
 
@@ -45,6 +44,7 @@
         }
     }
 
+    window.init = init
     async function loop(timestamp) {
         webcam.update(); // update the webcam frame
         await predict();
@@ -154,3 +154,24 @@
       var diff =(dt2.getTime() - dt1.getTime()) / 1000;
       return Math.abs(Math.round(diff));
   }
+
+  function pushNot() {
+    const push = require('web-push')
+
+    let vapidKeys = {
+        publicKey: 'BLcRzm0Z2FDmsCUX2H_rCo2B2bT9urwLaTcIRFimO3JvuKSn7GEOq-M6IUHv9H7CD-ErH6HH3y89Un0KV-CzV_w',
+        privateKey: 'yrYizttKpc5-cBnjdlsod8fZjBDSiy-FK_L3xqx-_64'
+    };
+
+    push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
+
+    let sub={endpoint:"https://fcm.googleapis.com/fcm/send/eqpcNJEyUvs:APA91bG_ktalnDDWbCk0TSJRaap308BGP72QvgbdAslS2AgUbn0JcE1OYWDftjoU_Kib7zTxT_N8hpWEGY8yCMfpYzrL8iSH2pwVxy6RAxojTBPTMgXGNbOY2qqkKEGYuTqzCusuypCy", 
+        expirationTimem: null, 
+        keys :{
+            p256dh:"BNi3YfHp7L9epncKdlbKiGRWyuTYxKB4wQD2PvXtYYOTPnReAfoYBe_aRXJlYKiqpkLMMWcqAwFLczHsS40tNPo",
+            auth:"5nrudkPjH_8IrGsEz1WZnw"
+        }
+    };
+
+    push.sendNotification(sub, 'testmessage');
+  };
